e139e4847fb8ccec4f6d7fb9594c408c22af1ee8
Seth Climenhaga <sethclim@gmail.com>
2021-06-19 12:09:31 -0400

    Layered Two Canvas

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -4,11 +4,26 @@ const Canvas = (props) => {
   const { draw, ...rest } = props;
   const canvasRef = useRef(null);
 
+  function resizeCanvasToDisplaySize(canvas) {
+    const { width, height } = canvas.getBoundingClientRect();
+
+    if (canvas.width !== width || canvas.height !== height) {
+      canvas.width = width;
+      canvas.height = height;
+      return true; // here you can return some usefull information like delta width and delta height instead of just true
+      // this information can be used in the next redraw...
+    }
+
+    return false;
+  }
+
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
 
     const render = () => {
+      console.log("Render called");
+      resizeCanvasToDisplaySize(canvas);
       draw(context);
     };
     render();
--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -3,12 +3,18 @@ import Canvas from "./canvas";
 import { useState } from "react";
 import { Point } from "../models/point";
 
+import "../css/graph.css";
+
 function Graph() {
   const [segments, setSegments] = useState(Array<Point>());
 
-  const draw = (ctx: any) => {
+  const debug = true;
+
+  const draw_LayerOne = (ctx: any) => {
     if (segments.length !== 0) {
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      //ctx.fillStyle = "blue";
+      //ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
       ctx.strokeStyle = "#ff0000";
       ctx.lineCap = "round";
       ctx.lineWidth = 5.0;
@@ -17,12 +23,22 @@ function Graph() {
       ctx.moveTo(segments[0].x, segments[0].y);
 
       for (var i = 1; i < segments.length; i++) {
-        console.log(segments[i].x, segments[i].y);
+        if (debug) console.log(segments[i].x, segments[i].y);
         ctx.lineTo(segments[i].x, segments[i].y);
       }
 
       ctx.stroke();
     }
+  };
+
+  const draw_LayerTwo = (ctx: any) => {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.beginPath();
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "blue";
+    ctx.rect(50, 50, 150, 80);
+    ctx.stroke();
   };
 
   const handleMouseDown = (event: any) => {
@@ -34,7 +50,23 @@ function Graph() {
     setSegments(segments.concat(point));
   };
 
-  return <Canvas onMouseDown={(e: any) => handleMouseDown(e)} draw={draw} />;
+  return (
+    <div id="graph">
+      <Canvas
+        id="foreground-layer"
+        onMouseDown={(e: any) => handleMouseDown(e)}
+        width="480"
+        height="320"
+        draw={draw_LayerOne}
+      />
+      <Canvas
+        id="background-layer"
+        draw={draw_LayerTwo}
+        width="480"
+        height="320"
+      />
+    </div>
+  );
 }
 
 export default Graph;
