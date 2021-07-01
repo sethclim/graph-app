ef95042865d243fa9c16845fef5d8b29ba3ac6bd
sethclim <56656904+sethclim@users.noreply.github.com>
2021-07-01 19:55:03 -0400

    restructured the layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import "./App.css";
-import "./components/graph";
-import Graph from "./components/graph";
+import "./css/base.css";
+import Graph from "./components/Graph";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainToolbar from "./components/MainToolbar";
 
 function App() {
   return (
-    <div className="App">
-      <Graph />
+    <div className="App" id="top-grid">
+      <Header className="header" />
+      <div className="content container">
+        <MainToolbar />
+        <Graph className="graph" />
+      </div>
+
+      <Footer className="footer" />
     </div>
   );
 }
---
+++ src/components/MainToolbar.js
@@ -0,0 +1,13 @@
+import "../css/maintoolbar.css";
+
+const MainToolbar = () => {
+  return (
+    <div id="main-toolbar">
+      <button>Pen</button>
+      <button>Dot</button>
+      <button>Eraser</button>
+    </div>
+  );
+};
+
+export default MainToolbar;
