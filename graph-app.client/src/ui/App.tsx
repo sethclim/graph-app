b14307abf3ca89e30e484a5f96a1a8c08083b570
sethclim <sethclim@gmail.com>
2024-12-30 11:02:32 -0500

    added lazy loading pages

--- graph-app.client/src/ui/App.tsx
+++ graph-app.client/src/ui/App.tsx
@@ -1,13 +1,18 @@
 import "./scss/base.scss";
 import "./scss/toplevel.scss";
 import Providers from "../domain/providers/index"
-
+import {lazy} from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+
+const Login = lazy(() => import("./pages/Login"))
+const Signup = lazy(() => import("./pages/Signup"))
+const SavedGraphs = lazy(() => import("./pages/savedgraphs/SavedGraphs"))
+
+// import Login from "./pages/Login";
 import AuthorizeRoute from "./components/utility/AuthorizeRoute";
-import Signup from "./pages/Signup";
-import SavedGraphs from "./pages/savedgraphs/SavedGraphs";
+// import Signup from "./pages/Signup";
+// import SavedGraphs from "./pages/savedgraphs/SavedGraphs";
 
 function App() {
 
